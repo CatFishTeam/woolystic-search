4da0cfadabb392896f1984408b7af856f9f250c2
Robilol <pimous78920@gmail.com>
2019-04-16 22:31:04 +0200

    MAEL JE TE HAIS :anger:

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 const csvtojsonV2 = require("csvtojson/v2");
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' })
+const {Client} = require('@elastic/elasticsearch')
+const client = new Client({node: 'http://localhost:9200'})
 
 
-
-client.indices.exists({index: 'wooly_gang'}, (err, res, status) => {
-    if (res) {
-        console.log('index already exists');
+client.indices.create({
+    index: 'wooly_gang'
+}, function (err, resp, status) {
+    if (err) {
+        console.log(err);
     } else {
-        client.indices.create({
-            index: 'wooly_gang'
-        }, function (err, resp, status) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("create", resp);
-            }
-        }).then(data => console.log(data))
-            .catch(err => console.log(err));
+        console.log("create", resp);
     }
-});
+})
 
 
 const converter = csvtojsonV2({
     noheader: true,
     trim: true,
-    delimiter: ';'
+    delimiter: ';',
+    quote: 'off'
 })
 
 const csvFilePath = './data.csv'
 converter
     .fromFile(csvFilePath)
     .then((jsonObj) => {
-        const body = jsonObj.map(row => {
-            return {
+
+        const body = [];
+
+        jsonObj.forEach(function (row) {
+            body.push({
+                "index":
+                    {
+                        "_index": "wooly_gang",
+                    }
+            })
+            body.push({
                 "name": row.field1,
                 "description": row.field2,
                 "url": row.field3,
@@ -42,17 +44,15 @@ converter
                 "category": row.field6,
                 "lang": row.field7,
                 "content": row.field8,
-            };
-        })
-
-        body.forEach((b) => {
-            client.index({
-                index: 'wooly_gang',
-                body: b
             })
-                .then(data => console.log(data))
-                .catch(err => console.log(err));
+        });
+
+        client.bulk({
+            body: body
         })
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+
     })
     .catch((error) => {
         console.log(error)
