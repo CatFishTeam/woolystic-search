57a8f31973adce407a4a040d42aca22b78756428
Robilol <robin.regis78@gmail.com>
2019-04-18 10:04:17 +0200

    setup

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const csvtojsonV2 = require("csvtojson/v2");
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' })
+require('dotenv').config()
+const {ClientElastic} = require('@elastic/elasticsearch')
+const clientElastic = new ClientElastic({node: 'http://localhost:9200'})
+const Twitter = require('twitter');
+const CoinMarketCap = require('coinmarketcap-api')
 
+const clientTwitter = new Twitter({
+    consumer_key: process.env.CONSUMER_API,
+    consumer_secret: process.env.CONSUMER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-client.indices.exists({index: 'wooly_gang'}, (err, res, status) => {
+const clientCMC = new CoinMarketCap(process.env.CMC_API_KEY)
+
+clientElastic.indices.exists({index: 'wooly_gang'}, (err, res, status) => {
     if (res) {
         console.log('index already exists');
     } else {
-        client.indices.create({
+        clientElastic.indices.create({
             index: 'wooly_gang'
         }, function (err, resp, status) {
             if (err) {
@@ -19,50 +29,3 @@ client.indices.exists({index: 'wooly_gang'}, (err, res, status) => {
             .catch(err => console.log(err));
     }
 });
-
-
-const converter = csvtojsonV2({
-    noheader: true,
-    trim: true,
-    delimiter: ';',
-    quote: 'off'
-})
-
-const csvFilePath = './data.csv'
-converter
-    .fromFile(csvFilePath)
-    .then((jsonObj) => {
-
-        const body = [];
-
-        jsonObj.forEach(function (row) {
-            body.push({
-                "index":
-                    {
-                        "_index": "wooly_gang",
-                    }
-            })
-            body.push({
-                "name": row.field1,
-                "description": row.field2,
-                "url": row.field3,
-                "author": row.field4,
-                "date": row.field5,
-                "category": row.field6,
-                "lang": row.field7,
-                "content": row.field8,
-            })
-        })
-
-        client.bulk({
-            body: body
-        })
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-
-    })
-    .catch((error) => {
-        console.log(error)
-    });
-
-
